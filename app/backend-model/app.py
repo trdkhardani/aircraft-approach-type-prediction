ede8d514509bdc4eefbd6a15e6b5ce46574f989e
Tridiktya Putra <tridiktya@gmail.com>
2025-06-03 07:36:54 +0700

    feat(app-backend): configuration for best models

--- app/backend-model/app.py
+++ app/backend-model/app.py
@@ -4,8 +4,11 @@ import pickle
 import numpy as np
 
 # load model
-with open("./trained-model/model.pkl", "rb") as f:
-    model = pickle.load(f)
+with open("./trained-model/model-xgb-ovr-BYS-with_airport_feats.pkl", "rb") as f:
+    model_with_airport_feats = pickle.load(f)
+
+with open("./trained-model/model-xgb-ovr-BYS-no_airport_feats.pkl", "rb") as f:
+    model_no_airport_feats = pickle.load(f)
 
 # create FastAPI app
 app = FastAPI()
@@ -21,13 +24,23 @@ def index():
         "message": "Hello from model!"
     }
 
-
 # prediction endpoint
-@app.post("/predict")
+@app.post("/predict/with-airport-feats")
 def predict(data: InputData):
     features_array = np.array(data.features).reshape(1, -1)
-    y_pred = model.predict(features_array)
-    y_proba = model.predict_proba(features_array)[0].tolist()
+    y_pred = model_with_airport_feats.predict(features_array)
+    y_proba = model_with_airport_feats.predict_proba(features_array)[0].tolist()
+    
+    return {
+        "prediction": y_pred.tolist(),
+        "probabilities": y_proba
+    }
+
+@app.post("/predict/no-airport-feats")
+def predict(data: InputData):
+    features_array = np.array(data.features).reshape(1, -1)
+    y_pred = model_no_airport_feats.predict(features_array)
+    y_proba = model_no_airport_feats.predict_proba(features_array)[0].tolist()
     
     return {
         "prediction": y_pred.tolist(),
