a9e59f0118af83d464abf9e84e900cd9e4a42290
Tridiktya Putra <tridiktya@gmail.com>
2026-04-22 13:05:16 +0700

    fix(get-atis): solved ambiguous module syntax

--- get-atis/airports_seed.js
+++ get-atis/airports_seed.js
@@ -1,48 +1,56 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-const airports = await prisma.airports.createMany({
-  data: [
-    {
-      airport_icao: 'KLAX'
-    },
-    {
-      airport_icao: 'KATL'
-    },
-    {
-      airport_icao: 'KIAD'
-    },
-    {
-      airport_icao: 'KORD'
-    },
-    {
-      airport_icao: 'KSFO'
-    },
-    {
-      airport_icao: 'KJFK'
-    },
-    {
-      airport_icao: 'KBOS'
-    },
-    {
-      airport_icao: 'KCMH'
-    },
-    {
-      airport_icao: 'KEWR'
-    },
-    {
-      airport_icao: 'KMIA'
-    },
-    {
-      airport_icao: 'KSEA'
-    },
-    {
-      airport_icao: 'KDEN'
-    },
-    {
-      airport_icao: 'KCLT'
-    },
-  ]
-})
+async function airportSeed() {
+  const airports = await prisma.airports.createMany({
+    data: [
+      {
+        airport_icao: 'KLAX'
+      },
+      {
+        airport_icao: 'KATL'
+      },
+      {
+        airport_icao: 'KIAD'
+      },
+      {
+        airport_icao: 'KORD'
+      },
+      {
+        airport_icao: 'KSFO'
+      },
+      {
+        airport_icao: 'KJFK'
+      },
+      {
+        airport_icao: 'KBOS'
+      },
+      {
+        airport_icao: 'KCMH'
+      },
+      {
+        airport_icao: 'KEWR'
+      },
+      {
+        airport_icao: 'KMIA'
+      },
+      {
+        airport_icao: 'KSEA'
+      },
+      {
+        airport_icao: 'KDEN'
+      },
+      {
+        airport_icao: 'KCLT'
+      },
+    ]
+  })
+}
 
-console.log(airports);
+airportSeed()
+.then(() => {
+  console.log('Airports seeded successfully!')
+})
+.catch(err => {
+  console.log(err)
+});
